ade93641a9061b2f256221a2805d79823d91763f
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 14:47:49 -0700

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quizly/core/routing/app_router.dart';
+import 'package:quizly/firebase_options.dart';
 import 'package:quizly/quizly.dart';
 
 void main() async{
   await ScreenUtil.ensureScreenSize();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Quizly(appRouter: AppRouter(),));
 }
 
