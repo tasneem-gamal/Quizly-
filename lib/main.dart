ae19b7ca87dc32f9adfcd942a64c0089797ac47a
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 13:40:47 -0700

    intialize screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quizly/core/routing/app_router.dart';
 import 'package:quizly/quizly.dart';
 
-void main() {
+void main() async{
+  await ScreenUtil.ensureScreenSize();
   runApp(Quizly(appRouter: AppRouter(),));
 }
 
--- lib/quizly.dart
+++ lib/quizly.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:quizly/core/routing/app_router.dart';
 import 'package:quizly/core/routing/routes.dart';
 import 'package:quizly/core/theme/colors.dart';
@@ -9,13 +10,17 @@ class Quizly extends StatelessWidget {
   final AppRouter appRouter;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light().copyWith(
-          scaffoldBackgroundColor: ColorsManager.mainBlue
-        ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: Routes.onBoarding,
-      onGenerateRoute: appRouter.generateRoutes,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        theme: ThemeData.light().copyWith(
+            scaffoldBackgroundColor: ColorsManager.mainBlue
+          ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoarding,
+        onGenerateRoute: appRouter.generateRoutes,
+      ),
     );
   }
 }
