dc2c00478d6db3bcd80dedf949ee5fd8a8b6b2dd
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 13:39:21 -0700

    add scaffold background color

--- lib/quizly.dart
+++ lib/quizly.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quizly/core/routing/app_router.dart';
 import 'package:quizly/core/routing/routes.dart';
+import 'package:quizly/core/theme/colors.dart';
 
 class Quizly extends StatelessWidget {
   const Quizly({super.key, required this.appRouter});
@@ -9,6 +10,9 @@ class Quizly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: ColorsManager.mainBlue
+        ),
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.onBoarding,
       onGenerateRoute: appRouter.generateRoutes,
