b40bb5b9a246fb2c9f92e9e8fb9ff6197b04a218
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 14:11:31 -0700

    create routes for home view

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizly/core/routing/routes.dart';
+import 'package:quizly/features/home/ui/home_view.dart';
 import 'package:quizly/features/onBoarding/ui/on_boarding_view.dart';
 
 class AppRouter {
@@ -8,6 +9,11 @@ class AppRouter {
       case Routes.onBoarding:
         return MaterialPageRoute(
           builder: (_) => const OnBoardingView()
+        );
+
+      case Routes.homeView:
+        return MaterialPageRoute(
+          builder: (_) => const HomeView()
         );
 
       default:
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,4 @@
 class Routes {
   static const onBoarding = '/onBoarding';
+  static const homeView = '/homeView';
 }
