97cd4286cfc2daf7a29cf053dd27972a80ba61a9
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 13:05:31 -0700

    add on boarding route

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:quizly/core/routing/routes.dart';
+import 'package:quizly/features/onBoarding/widgets/on_boarding_view.dart';
 
 class AppRouter {
   Route? generateRoutes(RouteSettings settings){
     switch(settings.name){
       case Routes.onBoarding:
         return MaterialPageRoute(
-          builder: (_) => SizedBox()
+          builder: (_) => const OnBoardingView()
         );
 
       default:
