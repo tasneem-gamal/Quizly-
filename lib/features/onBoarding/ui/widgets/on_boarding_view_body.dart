e9172ba412f1773586d7e001d0442d829b651ef9
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 14:00:51 -0700

    adding animation

--- lib/features/onBoarding/ui/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/ui/widgets/on_boarding_view_body.dart
@@ -2,24 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:quizly/core/helpers/spacing.dart';
 import 'package:quizly/core/theme/styles.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
 
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut)
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController, curve: const Interval(0.5, 1.0))
+    );
+
+    _animationController.forward();
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            'assets/images/book.png',
-            width: 200,
-            height: 200,
+          ScaleTransition(
+            scale: _scaleAnimation,
+            child: Image.asset(
+              'assets/images/book.png',
+              width: 200,
+              height: 200,
+            ),
           ),
           verticalSpace(10),
-          Text(
-            'Quizly',
-            style: CustomTextStyles.font24WhiteSemiBold(),
+          FadeTransition(
+            opacity: _fadeAnimation,
+            child: Text(
+              'Quizly',
+              style: CustomTextStyles.font24WhiteSemiBold(),
+            ),
           )
         ],
       ),
