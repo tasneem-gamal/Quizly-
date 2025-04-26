c39651689c553e8404a78ebc42187f5b803c48ac
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 12:39:11 -0700

    intialize app

---
+++ lib/quizly.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Quizly extends StatelessWidget {
+  const Quizly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quizly/main.dart';
+import 'package:quizly/quizly.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Quizly());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
