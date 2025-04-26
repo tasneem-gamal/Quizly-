3333a203ba3b77e91e5c315c6a88e1ca9199d248
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 14:17:38 -0700

    navigate to home

--- lib/features/onBoarding/ui/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/ui/widgets/on_boarding_view_body.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quizly/core/helpers/extension.dart';
+import 'package:quizly/core/routing/routes.dart';
 import 'package:quizly/features/onBoarding/logic/on_boarding_animation.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
 
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), (){
+      if (!mounted) return;
+      context.pushNamedAndRemoveUntil(Routes.homeView, (Route<dynamic> route) => false,);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return const Center(
