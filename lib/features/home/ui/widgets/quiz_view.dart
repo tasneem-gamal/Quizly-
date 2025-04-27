ec009d5ba80867e2f1a487bff12f02a1c33eca33
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-26 19:20:54 -0700

    adding app bar

--- lib/features/home/ui/widgets/quiz_view.dart
+++ lib/features/home/ui/widgets/quiz_view.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:quizly/core/helpers/extension.dart';
+import 'package:quizly/core/theme/colors.dart';
 
 class QuizView extends StatelessWidget {
   const QuizView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorsManager.mainBlue,
+        leading: IconButton(
+            onPressed: () {
+              context.pop();
+            },
+            icon: const Icon(
+              Icons.cancel_outlined,
+              size: 30,
+              color:Color.fromARGB(255, 237, 237, 237),
+            )),
+        title: CircleAvatar(radius: 20,),
+        centerTitle: true,
+      ),
+    );
   }
 }
