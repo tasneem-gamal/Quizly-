95300f74265fc7538687602b8f21cae7d646eb30
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-01 13:33:10 -0700

    refactor

--- lib/features/home/ui/home_view.dart
+++ lib/features/home/ui/home_view.dart
@@ -7,7 +7,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 250, 247, 219),
+      backgroundColor: Color.fromARGB(255, 252, 250, 230),
       body: SafeArea(child: HomeViewBody()),
     );
   }
