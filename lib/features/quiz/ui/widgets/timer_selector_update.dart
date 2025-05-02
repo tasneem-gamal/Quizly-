49b18d44b15141c84f41412cc6c3c50de96a7573
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-02 15:21:10 -0700

    handle timing

--- lib/features/quiz/ui/widgets/timer_selector_update.dart
+++ lib/features/quiz/ui/widgets/timer_selector_update.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quizly/core/helpers/extension.dart';
+import 'package:quizly/core/routing/routes.dart';
 import 'package:quizly/features/quiz/logic/quiz_provider.dart';
 import 'package:quizly/features/quiz/ui/widgets/circle_timer.dart';
 
@@ -17,12 +19,21 @@ class TimerSelectorUpdate extends StatelessWidget {
           return const SizedBox();
         }
         final currentQuestionNumber =
-        context.select<QuizProvider, int>((p) => p.currentQuestionNumber);
+        context.select<QuizProvider, int>((value) => value.currentQuestionNumber);
+        final totalQuestions = context.select<QuizProvider, int>((value) => value.totalQuestions);
+
         return CircleTimer(
           key: ValueKey(currentQuestionNumber),
           totalSeconds: 10,
           onTimeComplete: () {
-            context.read<QuizProvider>().nextQuestion();
+            final quizProvider = context.read<QuizProvider>();
+            if(currentQuestionNumber == totalQuestions){
+              if(context.mounted){
+                context.pushReplacementNamed(Routes.scoreView);
+              } 
+            } else {
+              quizProvider.nextQuestion();
+            }
           },
         );
       },
