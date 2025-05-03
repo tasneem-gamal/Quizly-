6412a07f54578ed4ea6ba70e9382cdb377c9ff77
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-03 15:00:52 -0700

    shuffle questions

--- lib/features/quiz/data/service/quiz_service.dart
+++ lib/features/quiz/data/service/quiz_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:quizly/features/quiz/data/models/quiz_model.dart';
+import 'package:quizly/features/quiz/data/models/quiz_question_model.dart';
 
 class QuizService {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
@@ -10,9 +11,19 @@ class QuizService {
           .collection('quizzes')
           .where('category', isEqualTo: category.trim())
           .get();
-      return response.docs.map((doc) {
-        return QuizModel.fromMap(doc.data());
-      }).toList();
+      if (response.docs.isEmpty) return [];
+
+      final quizModel = QuizModel.fromMap(response.docs.first.data());
+      final questions = List<QuizQuestion>.from(quizModel.questions)..shuffle();
+
+      final limitedQuestions = questions.take(10).toList();
+
+      return [
+        QuizModel(
+          category: quizModel.category,
+          questions: limitedQuestions,
+        )
+      ];
     } catch (e) {
       throw Exception('Error getting quizzes: $e');
     }
